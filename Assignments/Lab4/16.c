d411294f245ada616e9836d1c540ae598bf193f3
coder1097 <tungphan1097@gmail.com>
2019-11-21 00:02:54 +0700

    In progress

---
+++ Assignments/Lab4/16.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 100
+
+void getInputs(int [][N], int [][N],int,int);
+void add(int [][N], int [][N], int [][N],int,int);
+void print(int [][N],int,int);
+
+int main(){
+	int a[N][N], b[N][N], c[N][N];
+	int rows,cols;
+
+	printf("Rows and columns: ");
+	scanf("%d %d",&rows,&cols);
+
+	getInputs(a,b,rows,cols);
+	
+	printf("Matrix A:\n");
+	print(a,rows,cols);
+	printf("Matrix B:\n");
+	print(b,rows,cols);
+	
+	add(a,b,c,rows,cols);
+	printf("Output:\n");
+	print(c,rows,cols);
+}
+
+void getInputs(int a[][N], int b[][N], int rows, int cols){
+	srand(time(NULL));
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols;j++){
+			a[i][j] = rand()%1001;
+		}
+	}
+
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols;j++){
+			b[i][j] = rand()%101;
+		}
+	}
+}
+
+void add(int a[][N], int b[][N], int c[][N], int rows, int cols){
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols; j++){
+			c[i][j] = a[i][j]+b[i][j];
+		}
+	}
+}
+
+void print(int a[][N], int rows, int cols){
+
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols; j++){
+			printf(" %d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
